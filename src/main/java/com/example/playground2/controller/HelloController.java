a40ede41ff77e0363e5cdea381804ece34f6709a
sangcheol.byun <sangcheol.byun@gmail.com>
2021-12-28 02:31:10 +0900

    Hello, World! #11

--- src/main/java/com/example/playground2/controller/HelloController.java
+++ src/main/java/com/example/playground2/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "Hello, World! #2";
+        return "Hello, World! #11";
     }
 }
