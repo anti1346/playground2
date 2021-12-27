6ee1084fa3d4f30c01ad5ef000d012f675034a25
sangcheol.byun <sangcheol.byun@gmail.com>
2021-12-28 00:05:41 +0900

    first commit

---
+++ src/main/java/com/example/playground2/Playground2Application.java
@@ -0,0 +1,13 @@
+package com.example.playground2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Playground2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Playground2Application.class, args);
+	}
+
+}
