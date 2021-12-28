8f1469309ba627fcc0b34c2aabd194cb556e5655
eli.bf <42926329+anti1346@users.noreply.github.com>
2021-12-29 02:18:19 +0900

    Update HelloController.java

--- src/main/java/com/example/playground2/controller/HelloController.java
+++ src/main/java/com/example/playground2/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
-        return "Hello, World! #34";
+        return "Hello, World! #35";
     }
 }
